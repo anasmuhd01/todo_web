3db32daf0837cd81b853cbba19cce0c2258a2315
Anas Muhammed <ansmuhd01@gmail.com>
2023-07-09 16:40:08 +0530

    initial commit

---
+++ function.py
@@ -0,0 +1,10 @@
+
+def get_todos():
+    with open('todos.txt','r')as file:
+        todos_local = file.readlines()
+    return todos_local
+
+
+def write_todos(todos):
+    with open('todos.txt','w')as file:
+        file.writelines(todos)
---
+++ web.py
@@ -0,0 +1,23 @@
+import function
+import streamlit as st
+
+todos = function.get_todos()
+
+
+def add_todo():
+    todo = st.session_state['new_todo'] + "\n"
+    todos.append(todo)
+    function.write_todos(todos)
+
+
+st.title("To Do ✅")
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        function.write_todos(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
+
+st.text_input("Enter New TO DO ", on_change=add_todo, key='new_todo')
+# st.session_state
